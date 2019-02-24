fa7b29355dbb9c7b3b581c1ed9d1bbe938291a5f
smurf0969 <47907842+smurf0969@users.noreply.github.com>
2019-02-24 17:13:55 +0000

    Update ESP8266_Multi_Sensor.ino

--- ESP8266_Multi_Sensor.ino
+++ ESP8266_Multi_Sensor.ino
@@ -349,6 +349,8 @@ void sendMqtt(char* topic, char* msg, bool clientLoop) {
     client.loop();
   }
 #endif
+  Serial.print("Uptime: ");
+  Serial.println(getUptime());
 }
 
 // **************************************************
@@ -379,6 +381,33 @@ void sendTemperature(float &vTemperature) {
   sendMqtt(tTopicSn, tempChar, true);
 }
 
+String getUptime(){
+  unsigned long ms=millis();
+  long days = 0;
+  long hours = 0;
+  long mins = 0;
+  long secs = 0;
+  String secs_o = ":";
+  String mins_o = ":";
+  String hours_o = ":";
+  secs = ms / 1000; // set the seconds remaining
+  mins = secs / 60; //convert seconds to minutes
+  hours = mins / 60; //convert minutes to hours
+  days = hours / 24; //convert hours to days
+  secs = secs - (mins * 60); //subtract the coverted seconds to minutes in order to display 59 secs max
+  mins = mins - (hours * 60); //subtract the coverted minutes to hours in order to display 59 minutes max
+  hours = hours - (days * 24); //subtract the coverted hours to days in order to display 23 hours max
+  if (secs < 10) {
+    secs_o = ":0";
+  }
+  if (mins < 10) {
+    mins_o = ":0";
+  }
+  if (hours < 10) {
+    hours_o = ":0";
+  }
+  return days + hours_o + hours + mins_o + mins + secs_o + secs;
+}
 
 long lastThMsg = 0;
 long lastLdrMsg = 0;
